c41fbe69d7df11fff905d13ec440dce51ac76e04
Bill Chen <bill@Bills-MacBook-Pro.local>
2018-01-25 22:47:26 -0800

    updates

--- js/angularApp.js
+++
@@ -1,7 +0,0 @@
-(function(){
-    var app = angular.module('angular', [ ]);
-
-    app.controller("experienceController", function($scope){
-
-    });
-})();
--- js/experienceController.js
+++
@@ -1,7 +0,0 @@
-(function(){
-    angular.module('angular')
-    .controller("experienceController", function($scope){
-    console.log('working');
-
-    });
-})();
